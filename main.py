d61622cc61f62213f6f97a7be60715c16f655407
zhanghxiao <137479165+zhanghxiao@users.noreply.github.com>
2023-10-17 23:22:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
-from os import getenv
-
+# from os import getenv
+import os
 from src.pandora_cloud.server import ChatBot
 
 _port = getenv('PORT')
@@ -11,4 +11,11 @@ _local = getenv('PANDORA_LOGIN_LOCAL', 'false').lower() == 'true'
 _listen = getenv('PANDORA_SERVER_LISTEN', 'true').lower() == 'true'
 _server = getenv('PANDORA_SERVER', '0.0.0.0:{}'.format(_port) if _port else '0.0.0.0')
 
-app = ChatBot(proxy=_proxy, debug=_debug, login_local=_local).run(_server, listen=_listen)
+if __name__=='__main__':
+    app = ChatBot(proxy=_proxy, debug=_debug, login_local=_local).run(_server, listen=_listen)
+
+
+# from src.pandora_cloud.server import app
+
+# if __name__=='__main__':
+#     app.run(debug=True, port=os.getenv("PORT", default=5000), host='0.0.0.0')
